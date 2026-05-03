efd312c7684b23ce477d9c698c21683387139dbf
Kevin Riquelme <kriquelme10@gmail.com>
2026-05-03 01:41:10 +0000

    GTM load

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -2,11 +2,28 @@
 
 import Script from 'next/script';
 
+const GTM_ID = 'GTM-TKKS76ZH';
 const GA_MEASUREMENT_ID = 'G-LSZTLY8S35';
 
 export default function GoogleAnalytics() {
   return (
     <>
+      {/* Google Tag Manager - Head */}
+      <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `,
+        }}
+      />
+      
+      {/* gtag.js fallback - para eventos de ecommerce desde el código */}
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
@@ -19,19 +36,7 @@ export default function GoogleAnalytics() {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            
-            // Configuración básica con enhanced ecommerce
-            gtag('config', '${GA_MEASUREMENT_ID}', {
-              page_title: document.title,
-              page_location: window.location.href,
-              // Enhanced Ecommerce
-              send_page_view: true,
-              // Optimizaciones
-              debug_mode: ${process.env.NODE_ENV === 'development'}
-            });
-            
-            // Configurar linker para cross-domain si es necesario
-            gtag('set', 'cross_domain_state', 'UA-${GA_MEASUREMENT_ID.split('-')[1]}-1');
+            gtag('config', '${GA_MEASUREMENT_ID}');
           `,
         }}
       />

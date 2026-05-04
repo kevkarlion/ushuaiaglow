70c8361e5cbd24b84af5a95f87f0789d7ef6c862
Kevin Riquelme <kriquelme10@gmail.com>
2026-05-04 14:45:47 +0000

    fix: add Meta Pixel directly in layout head

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,7 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import { CartProvider } from '@/context/CartContext';
 import GoogleAnalytics from '@/components/GoogleAnalytics';
-import MetaPixel from '@/components/MetaPixel';
-import MetaPixelTracker from '@/components/MetaPixelTracker';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: {
@@ -29,9 +28,31 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+      <head>
+        {/* Meta Pixel base */}
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;
+              n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}
+              (window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+
+              fbq('init', '2716691235377865');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+      </head>
       <body className="min-h-screen flex flex-col">
-        <MetaPixel />
-        <MetaPixelTracker />
         <GoogleAnalytics />
         <CartProvider>
           <Navbar />

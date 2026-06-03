d7aa9895f0a98a77b349ff2a227ccffc09dc4116
Kevin Riquelme <kriquelme10@gmail.com>
2026-06-03 04:00:46 +0000

    add res.cloudinary.com to next/image remotePatterns

--- next.config.js
+++ next.config.js
@@ -14,6 +14,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'picsum.photos',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
     ],
   },
   // Fix for mobile navigation in Next.js 16
